7a1a0217a19763a15da0e9385b54f2efe217d54c
Chaosus <chaosus89@gmail.com>
2026-04-01 10:54:00 +0300

    fix

--- resources/statuses/weak.gd
+++ resources/statuses/weak.gd
@@ -1,16 +1,5 @@
 extends StatusResource
 
-
-func initialize_status():
-	status_changed.connect(_on_status_changed)
-	_on_status_changed()
-
-func _on_status_changed():
-	owner.damage_mult = 0.75
-
-func remove_status_effect():
-	owner.damage_mult = 1
-
 func update_text():
 	name = tr("st_weak_name")
 	des = tr("st_weak_in_game_des")
