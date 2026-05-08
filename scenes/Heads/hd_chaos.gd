5384d4c35cc8fb0cf435c8fab18828391e19c59e
Chaosus <chaosus89@gmail.com>
2026-05-08 12:11:29 +0300

    Доделана голова chaos

--- scenes/Heads/hd_chaos.gd
+++ scenes/Heads/hd_chaos.gd
@@ -12,30 +12,36 @@ func _ready() -> void:
 	super()
 
 
-func update_desc():
+func update_desc() -> void:
 	if invert_logic:
 		description = tr("hd_chaos_des_elite") % value
 	else:
-		description = tr("hd_chaos_des") % value
+		match level:
+			1:
+				description = tr("hd_chaos_des2") % value
+			2:
+				description = tr("hd_chaos_des3") % value
+			_:
+				description = tr("hd_chaos_des") % value
 
 
-func apply_passive_effect():
+func apply_passive_effect() -> void:
 	label.visible = true
 	label.text = str(value)
 
 
-func reroll_value():
+func reroll_value() -> void:
 	value = randi_range(1, 4)
 	label.text = str(value)
 	update_desc()
 	#update_labels()
 
 
-func head_selected():
+func head_selected() -> void:
 	label.visible = true
 
 
-func chaos(domino):
+func chaos(domino) -> void:
 	if last_domino == domino:
 		return
 	
@@ -49,15 +55,32 @@ func chaos(domino):
 	last_domino = null
 
 
-func add_action():
+func add_action() -> void:
 	var target = null
-	var damage := 0
+	var amount := 0
 	
 	if invert_logic:
 		target = Global.hero
-		damage = 2
+		amount = 2
 	else:
 		target = Global.enemy
-		damage = 4
+		amount = 4
+		match level:
+			1:
+				amount = 5
+			2:
+				amount = 6
 	
-	ActionManager.add(AttackAction.new(self, target, damage))
+	ActionManager.add(AttackAction.new(self, target, amount))
+	
+	if not invert_logic and level > 0:
+		var choices = ["fury", "thorns", "draw"]
+		var choice = choices.pick_random()
+		
+		match choice:
+			"fury":
+				ActionManager.add(BuffAction.new(self, Global.hero, StatusManager.fury, level))
+			"thorns":
+				ActionManager.add(BuffAction.new(self, Global.hero, StatusManager.thorns, level))
+			"draw":
+				ActionManager.add(BuffAction.new(self, Global.hero, StatusManager.draw, level))
