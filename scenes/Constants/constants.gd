75b518aa383dfede6d0692940bd7006dd3e87c7e
Aaron <denniksez@gmail.com>
2026-06-02 15:38:30 +0300

    Balance iter 4

--- scenes/Constants/constants.gd
+++ scenes/Constants/constants.gd
@@ -7,7 +7,7 @@ const MAX_POINTS = 4
 
 @export_subgroup("Berserk", "hd_berserk")
 @export_range(MIN_POINTS, MAX_POINTS) var hd_berserk_activator_value := 3
-@export var hd_berserk_damage_to_enemy := 5
+@export var hd_berserk_damage_to_enemy := 4
 @export var hd_berserk_damage_to_hero := 1
 @export var hd_berserk_fury_level_2 := 1
 @export var hd_berserk_fury_level_3 := 2
@@ -19,18 +19,18 @@ const MAX_POINTS = 4
 @export var hd_rock_armor_per_action_to_enemy := 1
 @export var hd_rock_armor_level_2 := 15
 @export var hd_rock_armor_level_3 := 45
-@export var hd_rock_health_decrement := 20
+@export var hd_rock_health_decrement := 15
 
 @export_subgroup("Chaos", "hd_chaos")
-@export var hd_chaos_damage_level_1 := 4
-@export var hd_chaos_damage_level_2 := 5
-@export var hd_chaos_damage_level_3 := 6
+@export var hd_chaos_damage_level_1 := 5
+@export var hd_chaos_damage_level_2 := 6
+@export var hd_chaos_damage_level_3 := 7
 @export var hd_chaos_damage_to_hero := 2
 
 @export_subgroup("Druid", "hd_druid")
-@export var hd_druid_fury_level_1 := 2
-@export var hd_druid_fury_level_2 := 2
-@export var hd_druid_fury_level_3 := 4
+@export var hd_druid_fury_level_1 := 3
+@export var hd_druid_fury_level_2 := 4
+@export var hd_druid_fury_level_3 := 5
 @export var hd_druid_crit_level_2 := 1
 @export var hd_druid_crit_level_3 := 1
 @export var hd_druid_fury_to_enemy := 1
@@ -91,9 +91,9 @@ const MAX_POINTS = 4
 @export var hd_construct_block_to_enemy := 5
 
 @export_subgroup("Warden", "hd_warden")
-@export var hd_warden_max_hp_increment_level_1 := 30
-@export var hd_warden_max_hp_increment_level_2 := 45
-@export var hd_warden_max_hp_increment_level_3 := 70
+@export var hd_warden_max_hp_increment_level_1 := 25
+@export var hd_warden_max_hp_increment_level_2 := 35
+@export var hd_warden_max_hp_increment_level_3 := 45
 @export var hd_warden_regen_level_2 := 4
 @export var hd_warden_regen_level_3 := 8
 
