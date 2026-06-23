f5bac9c64054b32985712feec8e7bcad7cf632a4
Chaosus <chaosus89@gmail.com>
2026-06-23 13:19:10 +0300

    Fix

--- scenes/BoardBonuses/NeutralBonuses/bb_n_remove_10_fury.gd
+++ scenes/BoardBonuses/NeutralBonuses/bb_n_remove_10_fury.gd
@@ -1,9 +1,9 @@
+@tool
 extends BoardBonus
 
-
-func add_action():
+func add_action() -> void:
 	ActionManager.add(DebuffAction.new(self, Global.enemy, StatusManager.fury, -10))
 	
-func update_labels(): 
-	tooltip_panel.caption = tr("bn_remove_strength_name")
-	tooltip_panel.description = TextFormatter.highlight_keywords(tr("bn_remove_strength_des") % abs(-10))
+func update_labels() -> void: 
+	tooltip_panel.caption = tr("BN_NTR_REMOVE_FURY_NAME")
+	tooltip_panel.description = TextFormatter.highlight_keywords(tr("BN_NTR_REMOVE_FURY_DESC") % abs(-10))
